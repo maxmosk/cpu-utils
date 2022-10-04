82c89ec4e72b8d70718b22ae4d9d18492ea6cb3f
Maxim <maxmosk@list.ru>
2022-10-04 09:02:36 +0300

    Loading and execution in main

--- cpu/src/code/main.c
+++ cpu/src/code/main.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include "cpu.h"
 
 
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (1 != argc)
+    {
+        printf("Error: invalid command line arguments. Please, read docs.\n");
+    }
+
     cpu_t myCPU = {0};
     cpuCtor(&myCPU);
-    
+    cpuLoad(&myCPU, argv[1]);
+    cpuExec(&myCPU);
     cpuDtor(&myCPU);
 
     return 0;
