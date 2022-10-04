a4e12bddb132e7f4d404ed9524a53e6f2698e344
Maxim <maxmosk@list.ru>
2022-10-04 10:05:20 +0300

    Finish code loader

--- cpu/src/code/cpu.c
+++ cpu/src/code/cpu.c
@@ -21,6 +21,27 @@ enum CPU_CODES cpuCtor(cpu_t *cpu)
 enum CPU_CODES cpuLoad(cpu_t *cpu, const char *codeFile)
 {
     long int codeSize = cpuFileSize(codeFile);
+
+    cpu->code = calloc(codeSize, 1);
+    if (NULL == cpu->code)
+    {
+        return CPU_ERROR;
+    }
+
+    FILE *exeFile = fopen(codeFile, "rb");
+    if (NULL == exeFile)
+    {
+        return CPU_ERROR;
+    }
+
+    size_t readed = fread(cpu->code, 1, codeSize, exeFile);
+    if (readed != codeSize)
+    {
+        return CPU_ERROR;
+    }
+
+    cpu->codeSize = codeSize;
+
     return CPU_SUCCESS;
 }
 
@@ -34,6 +55,9 @@ enum CPU_CODES cpuExec(cpu_t *cpu)
 enum CPU_CODES cpuDtor(cpu_t *cpu)
 {
     stackDtor(&(cpu->stack)) ASSERTED;
+
+    free(cpu->code);
+    cpu->codeSize = -1;
     
     return CPU_SUCCESS;
 }
--- cpu/src/code/main.c
+++ cpu/src/code/main.c
@@ -8,13 +8,14 @@ int main(int argc, char **argv)
     if (2 != argc)
     {
         printf("Error: invalid command line arguments. Please, read docs.\n");
+        return 0;
     }
 
     cpu_t myCPU = {0};
-    cpuCtor(&myCPU);
-    cpuLoad(&myCPU, argv[1]);
-    cpuExec(&myCPU);
-    cpuDtor(&myCPU);
+    cpuCtor(&myCPU) ASSERTED;
+    cpuLoad(&myCPU, argv[1]) ASSERTED;
+    cpuExec(&myCPU) ASSERTED;
+    cpuDtor(&myCPU) ASSERTED;
 
     return 0;
 }
