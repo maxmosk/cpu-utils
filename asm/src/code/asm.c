278931dc71e8011eba80718673ced24104c0c1de
Maxim <maxmosk@list.ru>
2022-10-06 16:27:26 +0300

    Started asm funcs

---
+++ asm/src/code/asm.c
@@ -0,0 +1,27 @@
+#include "asm.h"
+
+
+
+enum ASM_CODES asmCtor(asm_t *thisAsm)
+{
+    return ASM_SUCCESS;
+}
+
+
+enum ASM_CODES asmLoad(asm_t *thisAsm, const char *sourceFile)
+{
+    return ASM_SUCCESS;
+}
+
+
+enum ASM_CODES asmBuild(asm_t *thisAsm, const char *execFile)
+{
+    return ASM_SUCCESS;
+}
+
+
+enum ASM_CODES asmDtor(asm_t *thisAsm)
+{
+    return ASM_SUCCESS;
+}
+
