39d40657900c95a29e9169d218d9689eb66fa521
Maxim <maxmosk@list.ru>
2022-10-06 16:36:02 +0300

    Add ptr-checkers to asm

--- asm/src/code/asm.c
+++ asm/src/code/asm.c
@@ -2,26 +2,50 @@
 
 
 
+#define ASM_CHECK(cond, ret)                                                    \
+do                                                                              \
+{                                                                               \
+    bool tmpcond_ = (cond);                                                     \
+    assert(tmpcond_);                                                           \
+    if (!(tmpcond_))                                                            \
+    {                                                                           \
+        return ret;                                                             \
+    }                                                                           \
+}                                                                               \
+while (0)
+
+
+
 enum ASM_CODES asmCtor(asm_t *thisAsm)
 {
+    ASM_CHECK(NULL != thisAsm, ASM_ERROR);
+
     return ASM_SUCCESS;
 }
 
 
 enum ASM_CODES asmLoad(asm_t *thisAsm, const char *sourceFile)
 {
+    ASM_CHECK(NULL != thisAsm, ASM_ERROR);
+    ASM_CHECK(NULL != sourceFile, ASM_ERROR);
+
     return ASM_SUCCESS;
 }
 
 
 enum ASM_CODES asmBuild(asm_t *thisAsm, const char *execFile)
 {
+    ASM_CHECK(NULL != thisAsm, ASM_ERROR);
+    ASM_CHECK(NULL != execFile, ASM_ERROR);
+
     return ASM_SUCCESS;
 }
 
 
 enum ASM_CODES asmDtor(asm_t *thisAsm)
 {
+    ASM_CHECK(NULL != thisAsm, ASM_ERROR);
+
     return ASM_SUCCESS;
 }
 
