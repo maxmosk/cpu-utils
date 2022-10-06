e5e01ba77781ee4e58c069ee900bce070f8d10e6
Maxim <maxmosk@list.ru>
2022-10-07 01:59:11 +0300

    Add disasm reading file

--- disasm/src/code/disasm.c
+++ disasm/src/code/disasm.c
@@ -16,6 +16,10 @@ while (0)
 
 
 
+static long int dasmFileSize(const char *file);
+
+
+
 enum DASM_CODES disasmCtor(disasm_t *dasm)
 {
     DASM_CHECK(NULL != dasm, DASM_ERROR);
@@ -26,6 +30,26 @@ enum DASM_CODES disasmCtor(disasm_t *dasm)
 
 enum DASM_CODES disasmLoad(disasm_t *dasm, const char *codeFile)
 {
+    long int codeSize = dasmFileSize(codeFile);
+
+    dasm->code = calloc((size_t) codeSize, 1);
+    DASM_CHECK(NULL != dasm->code, DASM_ALLOCERR);
+
+    FILE *exeFile = fopen(codeFile, "rb");
+    DASM_CHECK(NULL != exeFile, DASM_FILEERR);
+
+    signature_t sign = {0};
+    size_t signReadStat = fread(&sign, 1, sizeof (signature_t), exeFile);
+    DASM_CHECK(sizeof (signature_t) == (size_t) signReadStat, DASM_FILEERR);
+    DASM_CHECK(signCheck(&sign), DASM_SIGNERR);
+
+    size_t codeReadStat = fread(dasm->code, 1, (size_t) codeSize - sizeof (signature_t), exeFile);
+    DASM_CHECK((size_t) codeReadStat == codeSize - sizeof (signature_t), DASM_FILEERR);
+    DASM_CHECK(0 == fclose(exeFile), DASM_FILEERR);
+
+    dasm->codeSize = codeSize / sizeof (cpuInstruction_t);
+
+    return DASM_SUCCESS;
 }
 
 
@@ -43,5 +67,15 @@ enum DASM_CODES disasmDtor(disasm_t *dasm)
     dasm->codeSize = LONG_MAX;
 
     return DASM_SUCCESS;
+}
+
+
+static long int dasmFileSize(const char *file)
+{
+    struct stat buf = {0};
+
+    DASM_CHECK(-1 != stat(file, &buf), -1);
+
+    return buf.st_size;
 }
 
--- disasm/src/include/disasm.h
+++ disasm/src/include/disasm.h
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "opcodes.h"
 #include "signature.h"
 
@@ -26,7 +27,8 @@ enum DASM_CODES
     DASM_SUCCESS  = 0,
     DASM_ERROR    = 1,
     DASM_ALLOCERR = 2,
-    DASM_SIGNERR  = 3
+    DASM_SIGNERR  = 3,
+    DASM_FILEERR  = 4
 };
 
 
