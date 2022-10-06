58dceb4c8739f419a13d698da431a4b756d51b2a
Maxim <maxmosk@list.ru>
2022-10-06 15:56:33 +0300

    Add awesome ASM main

---
+++ asm/src/code/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv)
+{
+    asm_t assem = {};
+    asmCtor(&assem) ASM_ASSERTED;
+
+    asmLoad(&assem, argv[1]) ASM_ASSERTED;
+    asmBuild(&assem, argv[2]) ASM_ASSERTED;
+
+    asmDtor(&assem) ASM_ASSERTED;
+
+    return 0;
+}
+
