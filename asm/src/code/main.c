29ed6ed2303643922cb99fb17530005bac857dd2
Maxim <maxmosk@list.ru>
2022-10-06 16:27:59 +0300

    Add cmd line args checker in main

--- asm/src/code/main.c
+++ asm/src/code/main.c
@@ -5,13 +5,21 @@
 
 int main(int argc, char **argv)
 {
-    asm_t assem = {};
+    if (2 != argc)
+    {
+        printf("Error: invalid command line arguments. Please, read docs.\n");
+        return 0;
+    }
+
+
+    asm_t assem = {0};
     asmCtor(&assem) ASM_ASSERTED;
 
     asmLoad(&assem, argv[1]) ASM_ASSERTED;
     asmBuild(&assem, argv[2]) ASM_ASSERTED;
 
     asmDtor(&assem) ASM_ASSERTED;
+
 
     return 0;
 }
