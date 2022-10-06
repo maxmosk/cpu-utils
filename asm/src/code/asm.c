50a84e7761f0dab81025dda5e08504621db40927
Maxim <maxmosk@list.ru>
2022-10-06 17:19:47 +0300

    Add sign writer

--- asm/src/code/asm.c
+++ asm/src/code/asm.c
@@ -30,6 +30,8 @@ enum ASM_CODES asmLoad(asm_t *thisAsm, const char *sourceFile)
     ASM_CHECK(NULL != sourceFile, ASM_ERROR);
 
     ASM_CHECK(SUCCESS == txtRead(&thisAsm->source, sourceFile), ASM_ERROR);
+    ASM_CHECK(NULL != (thisAsm->code = calloc(thisAsm->source.quan_lines, sizeof *thisAsm->code)), ASM_ERROR);
+
 
     return ASM_SUCCESS;
 }
@@ -39,6 +41,14 @@ enum ASM_CODES asmBuild(asm_t *thisAsm, const char *execFile)
 {
     ASM_CHECK(NULL != thisAsm, ASM_ERROR);
     ASM_CHECK(NULL != execFile, ASM_ERROR);
+
+    
+
+    FILE *exec = NULL;
+    ASM_CHECK(NULL != (exec = fopen(thisAsm, "wb")), ASM_ERROR);
+
+    signature_t sign = {CPU_EXE_FORMAT, CPU_EXE_VERSION};
+    ASM_CHECK(1 == fwrite(&sign, sizeof sign, 1, exec), (fclose(exec), ASM_ERROR));
 
     return ASM_SUCCESS;
 }
@@ -51,6 +61,7 @@ enum ASM_CODES asmDtor(asm_t *thisAsm)
     free(thisAsm->code);
     txtFree(&thisAsm->source);
     thisAsm->codeSize = SIZE_MAX;
+
 
     return ASM_SUCCESS;
 }
