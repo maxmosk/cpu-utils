5ac25736bbdd4e2faaed3b55f64019e75fb1563d
Maxim <maxmosk@list.ru>
2022-10-13 18:49:45 +0300

    Add POSIX to Makefile

--- include/commands.h
+++ include/commands.h
@@ -12,37 +12,35 @@ DEFCMD(PUSH, 0x01, 1,
             cpuNumber_t num = NAN;
 
             if (0 != cpu->code[cpu->pc].opcode.mem)
+            {
+                long long addr = 0;
+
+                if (0 != cpu->code[cpu->pc].opcode.imm)
                 {
-                    long long addr = 0;
-
-                    if (0 != cpu->code[cpu->pc].opcode.imm)
-                    {
-                        addr += cpu->code[cpu->pc].data.address;
-                    }
-                    if (0 != cpu->code[cpu->pc].opcode.reg)
-                    {
-                        addr += (long long) cpu->reg[cpu->code[cpu->pc].opcode.regNo];
-                    }
-
-                    num = cpu->RAM[addr];
-                    sleep(1);
+                    addr += cpu->code[cpu->pc].data.address;
+                }
+                if (0 != cpu->code[cpu->pc].opcode.reg)
+                {
+                    addr += (long long) cpu->reg[cpu->code[cpu->pc].opcode.regNo];
                 }
 
-                else
-                {
-                    if (0 != cpu->code[cpu->pc].opcode.imm)
-                    {
-                        num = cpu->code[cpu->pc].data.number;
-                    }
-                    else if (0 != cpu->code[cpu->pc].opcode.reg)
-                    {
-                        num = cpu->reg[cpu->code[cpu->pc].opcode.regNo];
-                    }
-                }
-
-                CPU_CHECK(STACK_ERROR != stackPush(&cpu->stack, cpu->code[cpu->pc].data.number), CPU_STACKERR);
-                break;
+                num = cpu->RAM[addr];
+                sleep(1);
             }
+
+            else
+            {
+                if (0 != cpu->code[cpu->pc].opcode.imm)
+                {
+                    num = cpu->code[cpu->pc].data.number;
+                }
+                else if (0 != cpu->code[cpu->pc].opcode.reg)
+                {
+                    num = cpu->reg[cpu->code[cpu->pc].opcode.regNo];
+                }
+            }
+
+            CPU_CHECK(STACK_ERROR != stackPush(&cpu->stack, cpu->code[cpu->pc].data.number), CPU_STACKERR);
         }
       )
 
@@ -161,12 +159,12 @@ DEFCMD(POP, 0xA0, 1,
             }
              
             CPU_CHECK(STACK_ERROR != stackPop(&cpu->stack, dst), CPU_STACKERR);
-        })
+        }
+      )
 
 
 DEFCMD(DUMP, 0x1F, 0,
         {
             cpuDump(cpu);
         })
-
 
