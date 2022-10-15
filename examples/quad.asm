86f439d1f60df7c08c21f3d78b454a7bc2a1a0ee
Maxim <maxmosk@list.ru>
2022-10-15 19:44:14 +0300

    Add linear solution in non-zero case

--- examples/quad.asm
+++ examples/quad.asm
@@ -6,14 +6,27 @@ hlt
 
 abs:            ; a
 dup             ; a a
-dup             ; a a a
-push 0          ; a a a 0
-jb :neg         ; a a
+push 0          ; a a 0
+jb :absneg      ; a
 ret
-neg:
-push -1 
-mul
+absneg:
+push -1         ; a -1
+mul             ; -a
 ret
+
+
+D:              ; rax = a, rbx = b, rcx = c
+push rbx        ; b
+dup             ; b b
+mul             ; b^2
+push 4          ; b^2 4
+push rax        ; b^2 4 a
+push rcx        ; b^2 4 a c
+mul             ; b^2 4 a*c
+mul             ; b^2 4*a*c
+sub             ; b^2-4*a*c
+ret
+
 
 solveQuad:      ; a b c
 pop rcx         ; a b
@@ -23,9 +36,31 @@ push rax        ; a
 push rbx        ; a b
 push rcx        ; a b c
 
-push rax        ; a b c a
+push rax        ; a b c
 call :abs       ; a b c |a|
-out
+push 0.001      ; a b c |a| 0
+jb :linear      ; a b c
 
+call :D
+
+ret
+
+
+linear:         ; rbx = b, rcx = c
+push rbx        ; b
+dup             ; b b
+call :abs       ; b |b|
+push 0          ; b |b| 0
+jb :zero_b      ; b
+pop rbx         ;
+push 0          ; 0
+push rcx        ; 0 c
+sub             ; -c
+push rbx        ; -c b
+div             ; -c/b
+out             ;
+ret
+
+zero_b:         ; 0
 ret
 
