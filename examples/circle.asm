6ff97e9055544b94b8e20bb83da34a69a155d22b
Maxim <maxmosk@list.ru>
2022-10-17 19:53:27 +0300

    New circle algo started

--- examples/circle.asm
+++ examples/circle.asm
@@ -5,29 +5,14 @@ pop rbx
 
 push 0          ; 0
 pop rcx         ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 y_start:
 
 push 0          ; 0
 pop rdx         ;
 x_start:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 push rdx        ; x
-
-push rax        ; x X_SIZE
-push rcx        ; x X_SIZE y
-mul             ; x X_SIZE*y
-push rdx        ; x X_SIZE*y x
-add             ; x X_SIZE*y+x
-pop rdx         ; x
-push 49         ; x 49
-pop [rdx]       ; x
-dup             ; x x
-pop rdx         ; x
-
-
-call :dist      ; x d
-push 5          ; x d 5
-ja :next_x      ; x
-
 push rax        ; x X_SIZE
 push rcx        ; x X_SIZE y
 mul             ; x X_SIZE*y
@@ -36,14 +21,13 @@ add             ; x X_SIZE*y+x
 pop rdx         ; x
 push 48         ; x 48
 pop [rdx]       ; x
-
-next_x:
 push 1          ; x 1
 add             ; x+1
 dup             ; x+1 x+1
 pop rdx         ; x+1
 push rax        ; x+1 X_SIZE
 jbe :x_start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 push rcx        ; y
 push 1          ; y 1
@@ -51,12 +35,31 @@ add             ; y+1
 dup             ; y+1 y+1
 pop rcx         ; y+1
 push rbx        ; y+1 Y_SIZE
-print
+print           ; y+1 Y_SIZE
 jbe :y_start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 print
 hlt
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+draw_circle:    ; rax = X_SIZE, rbx = Y_SIZE
+push 0          ; 0
+pop rcx
+push 10         ; R
+pop rdx
+
+
+
+ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 dist:           ; rcx=y, rdx=x
 push rcx        ; y
 push 12         ; y y0
@@ -72,5 +75,6 @@ mul             ; Dy Dx
 add             ; Dy+Dx
 sqrt            ; d
 ret
+
 
 
